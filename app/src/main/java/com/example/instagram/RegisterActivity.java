eb0771d3edd8189c0d0b8dddf8f4d712abd68402
ArthackA <bsclmr650118@spu.ac.ke>
2019-07-20 20:13:32 +0300

    Fixed register

--- app/src/main/java/com/example/instagram/RegisterActivity.java
+++ app/src/main/java/com/example/instagram/RegisterActivity.java
@@ -1,9 +1,5 @@
 package com.example.instagram;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -27,9 +26,8 @@ import java.util.HashMap;
 public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     DatabaseReference mReference;
-
     ProgressDialog mDialog;
-    EditText mUsername,mEmail,mPassword;
+    EditText mUsername, mEmail, mPassword;
     Button mRegister;
     TextView mMember;
 
@@ -39,17 +37,15 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mRegister=findViewById(R.id.reg_btn);
-        mMember = findViewById(R.id.txt_member);
-
         mAuth = FirebaseAuth.getInstance();
 
-        init();
+
+        initializer();
 
         mMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(RegisterActivity.this,LoginActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -58,81 +54,82 @@ public class RegisterActivity extends AppCompatActivity {
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name= mUsername.getText().toString().trim();
-                String email=mEmail.getText().toString().trim();
-                String password= mPassword.getText().toString().trim();
+                String name = mUsername.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
 
-                mDialog =new ProgressDialog(RegisterActivity.this);
-                mDialog.setMessage("please wait");
+                mDialog = new ProgressDialog(RegisterActivity.this);
+                mDialog.setMessage("please wait ");
                 mDialog.show();
 
-                if(!name.isEmpty()){
-                    if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                        if (!password.isEmpty()){
-                                if (password.length()>=6){
-                                    doRegister(name,email,password);
-                                }else {
-                                    mDialog.dismiss();
-                                    Toast.makeText(RegisterActivity.this, "Minimum password length should be atleast 6 characters", Toast.LENGTH_SHORT).show();
-                                }
-                        }else{
+                if (!name.isEmpty()) {
+                    if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        if (!password.isEmpty()) {
+                            if (password.length() >= 6) {
+                                doRegister(name, email, password);
+                            } else {
+                                mDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Minimum password length should be atleast 6 characters", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
                             mDialog.dismiss();
                             mPassword.setError("Password Required");
                             mPassword.requestFocus();
                         }
 
-                    }else {
+                    } else {
                         mDialog.dismiss();
                         mEmail.setError("Email Required");
                         mEmail.requestFocus();
                     }
-
-                }else {
+                } else {
                     mDialog.dismiss();
                     mUsername.setError("Username Required");
                     mUsername.requestFocus();
                 }
             }
-
         });
 
     }
-   public void init(){
-        mUsername=findViewById(R.id.reg_username);
-        mEmail=findViewById(R.id.reg_email);
-        mPassword=findViewById(R.id.reg_password);
 
+    public void initializer() {
+
+        mRegister = findViewById(R.id.reg_btn);
+        mMember = findViewById(R.id.txt_member);
+        mUsername = findViewById(R.id.reg_username);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
 
     }
 
     private void doRegister(final String name, final String email, final String password) {
-               mAuth.createUserWithEmailAndPassword(name,email)
+
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Toast.makeText(RegisterActivity.this, "Success", Toast.LENGTH_SHORT).show();
 
-                        if(task.isSuccessful()){
-                            FirebaseUser firebaseUser= mAuth.getCurrentUser();
-                            assert firebaseUser!=null;
-                            String userId= firebaseUser.getUid();
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userId = firebaseUser.getUid();
 
-                            mReference= FirebaseDatabase.getInstance().getReference("users").child(userId);
-                            HashMap<String,Object> myHashmap = new HashMap<>();
-                            myHashmap.put("id",userId);
-                            myHashmap.put("username",name);
-                            myHashmap.put("email",email);
-                            myHashmap.put("Bio","");
-                            myHashmap.put("imageUrl","https://firebasesto...-a4a0-2ed4a9130a5f");
+                            mReference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+                            HashMap<String, Object> mHashmap = new HashMap<>();
+                            mHashmap.put("id", userId);
+                            mHashmap.put("username", name);
+                            mHashmap.put("email", email);
+                            mHashmap.put("Bio", "");
+                            mHashmap.put("imageUrl", "https://firebasestorage.googleapis.com/v0/b/instagram-f76f2.appspot.com/o/account.png?alt=media&token=2bea5d03-2f62-402a-95b5-4cf573f19ffb");
 
-                            mReference.setValue(myHashmap).addOnCompleteListener(
-                                    new OnCompleteListener<Void>() {
+                            mReference.setValue(mHashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
+                                    if (task.isSuccessful()) {
                                         Toast.makeText(RegisterActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(RegisterActivity.this,HomeActivity.class));
+                                        startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
                                         finish();
                                         mDialog.dismiss();
                                     }
@@ -141,7 +138,7 @@ public class RegisterActivity extends AppCompatActivity {
                             });
 
 
-                        }else{
+                        } else {
                             Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
                             mDialog.dismiss();
                         }
